83453dc1188da4be730b405fec9118a4b59c2313
lasar <lsarmiento9311@bths.edu>
2022-09-27 16:23:13 -0400

    Added a new variable, totalTipAmount, which stores the total tip amount for the amount of people given.  Added three "System.out.print" statemenst that print out the tipPerPerson and the totalTipAmount to the user.

--- src/Unit1Project.java
+++ src/Unit1Project.java
@@ -9,8 +9,12 @@ public class Unit1Project {
         Integer peopleNum  = myObj.nextInt();
         System.out.println("How much do you want to tip?"); // asks user for the tip amount
         double tipAmount = (double) myObj.nextInt() / 100; // converts given value into a % number
-        double splitBill = totalBill / peopleNum;
+        double splitBill = totalBill / peopleNum; // divides totalBill by peopleNum to get the split bill, then puts value into variable
         double tipPerPerson = splitBill * tipAmount;
-        double totalPerPerson = tipPerPerson + splitBill;
+        double totalTipPerPerson = tipPerPerson + splitBill;
+        double totalTipAmount = tipPerPerson * peopleNum;
+        System.out.println("Each person must tip " + tipPerPerson);
+        System.out.print("This means that the total ");
+        System.out.println("tip amount is " + totalTipAmount);
     }
 }
