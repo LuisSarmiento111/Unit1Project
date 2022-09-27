37112e7989fca891721bd1a59f401453eb943516
lasar <lsarmiento9311@bths.edu>
2022-09-27 18:47:24 -0400

    Added comments to clarify what certain pieces of code do and its purpose to the overall goal of the code.

--- src/Unit1Project.java
+++ src/Unit1Project.java
@@ -2,30 +2,30 @@ import java.util.Scanner;
 
 public class Unit1Project {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("How much was the total bill?"); // asks user for total bill
-        double totalBill = myObj.nextDouble();
-        System.out.println("How many people were there?"); // asks user for amount of people
-        Integer peopleNum  = myObj.nextInt();
-        System.out.println("How much do you want to tip?"); // asks user for the tip amount
+        Scanner myObj = new Scanner(System.in); // creates scanner
+        System.out.println("How much was the total bill?");
+        double totalBill = myObj.nextDouble(); // puts given value into variable, totalBill
+        System.out.println("How many people were there?");
+        Integer peopleNum  = myObj.nextInt(); // puts given value into variable, peopleNum
+        System.out.println("How much do you want to tip?");
         double tipAmount = (double) myObj.nextInt() / 100; // converts given value into a % number
-        double splitBill = totalBill / peopleNum; // divides totalBill by peopleNum to get the split bill, then puts value into variable
-        double tipPerPerson = splitBill * tipAmount;
-        double roundedTipPerPerson = Math.round(tipPerPerson*100)/100d;
-        double totalCostPerPerson = tipPerPerson + splitBill;
-        double roundedTotalCostPerPerson = Math.round(totalCostPerPerson*100)/100d;
-        double totalTipAmount = tipPerPerson * peopleNum;
-        double roundedTotalTipAmount = Math.round(totalTipAmount*100)/100d;
-        double totalCostWithTip = totalBill + totalTipAmount;
-        double roundedTotalCostWithTip = Math.round(totalCostWithTip*100)/100d;
+        double splitBill = totalBill / peopleNum; // divides total bill by number of people to find the split bill
+        double tipPerPerson = splitBill * tipAmount; // multiplies split bill with tip amount to find the tip per person
+        double roundedTipPerPerson = Math.round(tipPerPerson*100)/100d; // rounds to the hundredths place
+        double totalCostPerPerson = tipPerPerson + splitBill; // finds total cost per person using tipPerPerson and splitBill
+        double roundedTotalCostPerPerson = Math.round(totalCostPerPerson*100)/100d; // rounds to the hundredths place
+        double totalTipAmount = tipPerPerson * peopleNum; // finds total tip amount using tipPerPerson and peopleNum
+        double roundedTotalTipAmount = Math.round(totalTipAmount*100)/100d; // rounds to the hundredths place
+        double totalCostWithTip = totalBill + totalTipAmount; // adds total bill with total tip amount to find total cost of everything
+        double roundedTotalCostWithTip = Math.round(totalCostWithTip*100)/100d; // rounds to the hundredths place
         System.out.println(" ");
-        System.out.println("Each person must tip $" + String.format("%.2f", roundedTipPerPerson));
+        System.out.println("Each person must tip $" + String.format("%.2f", roundedTipPerPerson)); // prints out the tip per person
         System.out.print("This means that the total ");
-        System.out.println("tip amount is $" + String.format("%.2f", roundedTotalTipAmount));
+        System.out.println("tip amount is $" + String.format("%.2f", roundedTotalTipAmount));  // prints out total tip amount
         System.out.print("The total cost per person ");
-        System.out.println("is $" + String.format("%.2f", roundedTotalCostPerPerson));
+        System.out.println("is $" + String.format("%.2f", roundedTotalCostPerPerson)); // prints total cost per person
         System.out.print("The total cost including the ");
-        System.out.println("tip is $" + String.format("%.2f", roundedTotalCostWithTip));
+        System.out.println("tip is $" + String.format("%.2f", roundedTotalCostWithTip)); // prints total cost including the tip
 
     }
 }
